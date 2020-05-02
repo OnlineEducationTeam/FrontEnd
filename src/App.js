67b5575d960f0c80290d655e944492b0a8b8224c
thenewArtist18 <biolaris3@gmail.com>
2020-05-02 17:01:11 +0300

    Revert "add App.css,App.js,Login.js,Login.css,Signup.js,Signup.css"
    This reverts commit 899b0e20639abcc0f714f5050dba7f2c6ab1e939.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from "react";
-import "./App.css";
-import Login from "./login";
-import SignUp from "./signup";
-import HomePage from "./homepage";
-import PersonOnline from "./homepagecomp/persononline";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import underConstruction from "./homepagecomp/underConstruction";
-import studentProfile from "./studentProfile";
-import educatorProfile from "./educatorProfile";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/OnliEdu/" exact component={Login} />
-        <Route path="/OnliEdu/signup" component={SignUp} />
-        <Route
-          path="/OnliEdu/homepagecomp/personOnline"
-          component={PersonOnline}
-        />
-        <Route path="/OnliEdu/homepage" component={HomePage} />
-        <Route
-          path="/OnliEdu/underConstruction"
-          component={underConstruction}
-        />
-        <Route path="/OnliEdu/studentProfile" component={studentProfile} />
-        <Route path="/OnliEdu/educatorProfile" component={educatorProfile} />
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
